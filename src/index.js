236bab5b9e45af0d22400c778f368733c7e26b80
vivekboyidapu <102375767+boyidapuvivek@users.noreply.github.com>
2025-07-10 14:15:55 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
-import express from "express";
 import "dotenv/config";
 import ConnectDB from "./db/index.js";
+import { app } from "./app.js";
 
-const app = express();
-
-// app.use(cors());
-ConnectDB();
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-// const rideRoutes = require("./routes/rideRoutes");
-
-// dotenv.config();
-// const app = express();
-
-// // Middleware
-// app.use(express.json());
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-
-// // MongoDB Connection
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB Connected Locally"))
-//   .catch((err) => console.log(err));
-
-// // Routes
-// app.use("/api/rides", rideRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+ConnectDB()
+  .then(() => {
+    app.listen(process.env.port || 8000, () => {
+      console.log(`Server is listning on port ${process.env.port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed : ", err);
+  });
