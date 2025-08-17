bb60cf5bac6bdb9c312d5ec8d0ade0e1aa79acc6
vivekboyidapu <102375767+boyidapuvivek@users.noreply.github.com>
2025-08-17 17:11:56 +0530

    Fix server port configuration and clean up code formatting in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import "dotenv/config";
-import ConnectDB from "./db/index.js";
-import { app } from "./app.js";
+import "dotenv/config"
+import ConnectDB from "./db/index.js"
+import { app } from "./app.js"
 
 ConnectDB()
   .then(() => {
-    app.listen(process.env.port || 8000, () => {
-      console.log(`Server is listning on port ${process.env.port}`);
-    });
+    app.listen(5000 || process.env.port, () => {
+      console.log(`Server is listning on port ${process.env.port}`)
+    })
   })
   .catch((err) => {
-    console.log("MONGO DB connection failed : ", err);
-  });
+    console.log("MONGO DB connection failed : ", err)
+  })
